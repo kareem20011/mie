25d80803fa6e8c313c0187584ec572c1ec31758d
kareem <kareemashraf20011@gmail.com>
2025-02-24 02:48:18 +0200

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('contents')
+
+
+<div class="title-group mb-3">
+    <h1 class="h2 mb-0">Overview</h1>
+
+    <small class="text-muted">Hello Thomas, welcome back!</small>
+</div>
+
+
+
+
+@endsection
